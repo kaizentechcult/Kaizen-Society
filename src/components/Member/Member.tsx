75bd77316679d24ad89223a33a3f37996621db25
sooraj002 <soorajnambiar8@gmail.com>
2024-11-22 19:22:11 +0530

    added something remaining refactoring

--- src/components/Member/Member.tsx
+++ src/components/Member/Member.tsx
@@ -1,30 +1,30 @@
 import Image from "next/image";
 import React from "react";
 
-type membersData = {
-  name: string;
-  post: string;
-  img: string;
-  github?: string;
-  linkedin?: string;
-};
+// type membersData = {
+//   name: string;
+//   post: string;
+//   img: string;
+//   github?: string;
+//   linkedin?: string;
+// };
 
 const Member = (membersData: any) => {
   console.log(membersData);
   return (
     <div className="flex items-center p-4 sm:w-2/5 w-full md:w-[20rem] border border-[#e5e7eb] m-4 rounded-lg">
       <div className="flex items-center">
-        {/* <Image
+        <Image
           src={membersData.img}
           alt="img"
           width={150}
           height={150}
           className="rounded-full"
-        /> */}
+        />
       </div>
       <div className="text-center flex flex-col gap-2 md:items-center justify-end md:justify-center w-full">
         <p className="font-semibold">{membersData.name}</p>
-        <p className="text-gray-900">{membersData.post}</p>
+        <p className="text-gray-900">{membersData.position}</p>
         <div className="flex gap-10 justify-center">
           {membersData.github && (
             <a target="_blank" aria-label="Github" href={membersData.github}>
