22a622e1cbd504985a86eaf8f89ccacdc5027f92
sooraj <soorajnambiar8@gmail.com>
2025-03-13 00:22:14 +0530

    added page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+
+
+const BlogPage = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-4xl font-bold">Blog Page</h1>
+        </div>
+    );
+};
+
+export default BlogPage;
